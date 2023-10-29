c24cabd975a2f6a8545fc720dde2e407240177ff
Giuseppe Brentino <brentinogiuseppe@gmail.com>
2023-10-29 17:00:57 +0100

    fixed bug on initial conditions iteration + ex.3

--- periodicOrbit.m
+++ periodicOrbit.m
@@ -41,31 +41,31 @@ iter    = 0;    % Iteration counter
 tol     = 1e-14; % Desired tolerance
 
 % Set the update to the first guess 
-vy0_new = vy0;
-x0_new  = x0;
-vxf_ref = 0;
-vzf_ref = 0;
-tic
+
 while (abs(err_vxf)>tol || abs(err_vzf)>tol) && iter < Nmax
     % Vector of initial conditions
+    if iter
+        phi = reshape(xx(7:end),6,6);
+        phi_p = [phi(4,1), phi(4,5);phi(6,1), phi(6,5)];
+
+        guess = [x0_new;vy0_new] - phi_p\[xx(4);xx(6)];
+
+        x0_new = guess(1);
+        vy0_new = guess(2);
+    else
+        vy0_new = vy0;
+        x0_new  = x0;
+    end
     xx0 = [x0_new;y0;z0;vx0;vy0_new;vz0;phi0];
     opt = odeset('RelTol',2.22045e-14,'AbsTol',1e-15,'Events',@(t,xx)planeCrossingEvent(t,xx));
     [~,xx] = ode113(@(t,xx)CRTBP(t,xx,mu),[0,100],xx0,opt);
     xx = xx(end,:);
-
-    phi = reshape(xx(7:end),6,6);
-    phi_p = [phi(4,1), phi(4,5);phi(6,1), phi(6,5)];
-
-    guess = [x0_new;vy0_new] - phi_p\[xx(4);xx(6)];
-
-    x0_new = guess(1);
-    vy0_new = guess(2);
+    
     err_vxf = xx(4);
     err_vzf = xx(6);
+   
     iter = iter + 1;
 end
-
- xx0 = [x0_new;y0;z0;vx0;vy0_new;vz0;phi0];
     opt = odeset('RelTol',2.22045e-14,'AbsTol',1e-15);
     [~,xx] = ode113(@(t,xx)CRTBP(t,xx,mu),[0,15],xx0,opt);
 
@@ -77,6 +77,83 @@ end
     axis equal
     xlabel('x')
     ylabel('y')
+
+%% Ex 3
+clearvars; close all; clc;
+mu = 0.012150;
+
+% Initial conditions
+x0  = 1.08892819445324;
+y0  = 0;
+z0  = 0.0591799623455459;
+vx0 = 0;
+vy0 = 0.257888699435051;
+vz0 = 0;
+phi0 = reshape(eye(6),36,1);
+
+zz0 = linspace(z0,0.034,10);
+vy0_new = vy0;
+x0_new  = x0;
+z0_length = length(zz0);
+
+tol     = 1e-14; % Desired tolerance
+Nmax    = 100;   % Maximum number of iterations
+x0_vect = zeros(2,z0_length);
+opt_plot = odeset('RelTol',2.22045e-14,'AbsTol',1e-15);
+
+figure()
+hold on
+grid on
+axis equal
+xlabel('x')
+ylabel('y')
+zlabel('z')
+plot3(-mu,0,0,'o','MarkerSize',20,'MarkerFaceColor','b','MarkerEdgeColor','b');
+plot3(1-mu,0,0,'o','MarkerSize',10,'MarkerFaceColor',[0.4 0.4 0.4],'MarkerEdgeColor',[0.4 0.4 0.4]);
+for i = 1:z0_length
+% correction of initial states with Pseudo-Newton method
+% Initialization
+err_vxf = 1;    % Initial guess of error (higher than tolerance)
+err_vzf = 1;    % Initial guess of error (higher than tolerance)
+ 
+iter    = 0;    % Iteration counter 
+
+while (abs(err_vxf)>tol || abs(err_vzf)>tol) && iter < Nmax
+    % Vector of initial conditions
+    if iter
+        phi = reshape(xx(7:end),6,6);
+        phi_p = [phi(4,1), phi(4,5);phi(6,1), phi(6,5)];
+
+        guess = [x0_new;vy0_new] - phi_p\[xx(4);xx(6)];
+
+        x0_new = guess(1);
+        vy0_new = guess(2);
+    else
+        vy0_new = vy0;
+        x0_new  = x0;
+    end
+    xx0 = [x0_new;y0;zz0(i);vx0;vy0_new;vz0;phi0];
+    opt = odeset('RelTol',2.22045e-14,'AbsTol',1e-15,'Events',@(t,xx)planeCrossingEvent(t,xx));
+    [~,xx] = ode113(@(t,xx)CRTBP(t,xx,mu),[0,100],xx0,opt);
+    xx = xx(end,:);
+    
+    err_vxf = xx(4);
+    err_vzf = xx(6);
+   
+    iter = iter + 1;
+end
+if iter<Nmax
+x0 = x0_new;
+vy0 = vy0_new;
+x0_vect(:,i) = [x0;vy0];
+else
+    error("The algorithm didn't converge")
+end
+% post process
+    [~,xx] = ode113(@(t,xx)CRTBP(t,xx,mu),[0,15],xx0,opt_plot);
+    plot3(xx(:,1),xx(:,2),xx(:,3));
+    plot3(xx(1,1),xx(1,2),xx(1,3),'o','MarkerSize',5,'MarkerFaceColor','r');
+end
 %% functions
 
 function dx = CRTBP(~,xx,mu)
